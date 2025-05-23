d9a2755ba02e489aa4f37bfa3ef3195ff075c9ed
NogalRootNick <nogalrootnick1@gmail.com>
2025-05-23 16:47:55 -0400

    Update codigo_del_robot.ino

--- codigo_del_robot.ino
+++ codigo_del_robot.ino
@@ -3,3 +3,6 @@
 |      CODIGO DEL ROBOT PARA VER EN LA COMPETENCI      |
 ========================================================
 **/
+void setup() {
+  Serial.println("Aqui estara proximamente!");
+}
