46a8655bb98d9fb1fedd6e079466e6d8763fef8c
NogalRootNick <nogalrootnick1@gmail.com>
2025-06-06 18:13:35 -0400

    Update Prueba_de_Coordenadas.ino

--- Prueba_de_Coordenadas/Prueba_de_Coordenadas.ino
+++ Prueba_de_Coordenadas/Prueba_de_Coordenadas.ino
@@ -1,35 +1,3 @@
-/*
-|==========================|
-|  CODIGO VIEJO DE TESTEO  |
-|==========================|
-
-float x = 1.4;
-float y = 1.02;
-
-void setup() {
-  Serial.begin(9600);
-  delay(50);
-}
-
-void loop() {
-  for (float coordValue = 0.00; coordValue < 10.00; coordValue += 0.1) {
-    Serial.print("Coordenadas: ");
-    Serial.print(coordValue);
-    Serial.print(" , ");
-    Serial.println(coordValue);
-
-    delay(100);
-  }
-  delay(1000);
-}*/
-
-
-/*
-|==========================|
-|  CODIGO NUEVO DE TESTEO  |
-|==========================|
-*/
-
 float x = 0.55;
 float y = 0.23;
 
